d2950e4979767da5a470832cbda62d3adc66dfb9
unknown <patilbhoomika6@gmail.com>
2021-08-29 19:42:07 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,4 +2,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-// app.use(bodyParser
+app.use(bodyParser.urlencoded({extended : true }));
+
+app.listen(3000 , ()=>{
+    console.log("Death star ready at port: 3000");
+});
+
+app.get("/" , (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+});
