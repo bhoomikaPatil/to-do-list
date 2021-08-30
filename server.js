73432e51b26541a274b1f071d7f6f1b51f63ab1f
himanshu udupi <himanshuudupi365@outlook.com>
2021-08-30 23:42:49 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
 app.use(bodyParser.urlencoded({extended : true }));
-
+app.set('view engine' , 'ejs');
+app.use(express.static("public"));
 app.listen(3000 , ()=>{
     console.log("Death star ready at port: 3000");
 });
 
 app.get("/" , (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html");
+    res.render(__dirname + "/views/home.ejs");
 });
