7a7b81e9189fdaf4db0aa27a58385ea2bba2fb6d
himanshu udupi <himanshuudupi365@outlook.com>
2021-09-01 12:24:28 +0530

    Partial refactoring(add comments), failure page added

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("https");
 const app = express();
+// Declaring the ipAddress
 let ipAddress = "";
+// Init body-parser
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// setting up ejs
 app.set('view engine', 'ejs');
+// instructing to serve static files from "public"
 app.use(express.static("public"));
+// listen at port 3000
 app.listen(3000, () => {
     console.log("Death star ready at port: 3000");
 });
-
+// actions for get at home route
 app.get("/", (req, res) => {
+    // making url for request to api server
     const api_key = "at_8NyQVzQfhXktHpx7UsBQ6oQLXyKEH";
     const api_url = 'https://geo.ipify.org/api/v1?';
     let url = "";
@@ -23,8 +29,11 @@ app.get("/", (req, res) => {
     } else {
         url += api_url + 'apiKey=' + api_key + '&ipAddress=' + ipAddress;
     }
+    // requesting info from url
     http.get(url, (response) => {
+        // logging status code
         console.log(response.statusCode);
+        // retriving data
         response.on("data", (data) => {
             const ipAddressInfo = JSON.parse(data);
             // console.log(ipAddressInfo);
@@ -39,6 +48,7 @@ app.get("/", (req, res) => {
                 lat: ipAddressInfo.location.lat,
                 lng: ipAddressInfo.location.lng
             };
+            // init object to send to home.ejs
             const ipInfo = {
                 ip: ip,
                 location: location,
@@ -46,13 +56,16 @@ app.get("/", (req, res) => {
                 isp: isp,
                 cordinates: cordinates
             };
+            // render home.ejs
             res.render("home.ejs", {
                 ipInfo: ipInfo
             });
         });
     });
 });
+// action for post at home route
 app.post("/", (req, res) => {
+    // init ip Address
     ipAddress = req.body.ipEntered;
     if(ipAddress === ""){
         res.render("failure");
@@ -61,7 +74,7 @@ app.post("/", (req, res) => {
         res.redirect("/");
     }
 });
-
+// action for post >> failure
 app.post("/failure", (req,res)=>{
     res.redirect("/");
 });
